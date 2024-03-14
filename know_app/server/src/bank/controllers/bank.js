ff14681a6882f9c9c7ad5c2469f35c603fae64cd
Vivek Pathak <vivekpathak2083@gmail.com>
2024-03-14 22:42:30 +0530

    fetch list controller added in backend

--- know_app/server/src/bank/controllers/bank.js
+++ know_app/server/src/bank/controllers/bank.js
@@ -7,10 +7,22 @@ const fetchTemplate = async (req, res) => {
   // TODO: Add the logic to handle the request
   try {
     const { bankName } = req.body;
-    const allBkTemplates = await Bank.findOneAndDelete({ bankName: bankName });
+    const allBkTemplates = await Bank.find({ bankName: bankName });
     return res
       .status(200)
-      .json(responseSchema(true, "user found", allBkTemplates));
+      .json(responseSchema(true, "templates fetched", allBkTemplates));
+  } catch (error) {
+    return res.status(400).json(responseSchema(false, error.toString()));
+  }
+};
+const fetchListTemplate = async (req, res) => {
+  // TODO: Add the logic to handle the request
+  try {
+    const { bankNames } = req.body;
+    const allBkTemplates = await Bank.find({ bankName: { $in: bankNames } });
+    return res
+      .status(200)
+      .json(responseSchema(true, "templates fetched", allBkTemplates));
   } catch (error) {
     return res.status(400).json(responseSchema(false, error.toString()));
   }
@@ -32,39 +44,61 @@ const addTemplate = async (req, res) => {
   // TODO: Add the logic to handle the request
   try {
     const { bankName, message } = req.body;
-    const apiResponse = "";
-    const rxPattern = "";
-    const propMap = "";
-    for (let i = 0; i < 10; i++) {
-      apiResponse = response(message);
-      rxPattern = regexPattern(apiResponse);
-      propMap = propertyMap(apiResponse);
+    let apiResponse = "";
+    let rxPattern = "";
+    let propMap = "";
+    for (let i = 0; i < 4; i++) {
+      apiResponse = await response(message);
+      console.log("api response done");
+      rxPattern = await regexPattern(apiResponse);
+      console.log("rx pattern done");
+      propMap = await propertyMap(apiResponse);
+      console.log("promap done");
 
+      console.log(rxPattern);
       const pattern = RegExp(rxPattern);
+      console.log(pattern);
       const match = pattern.exec(message);
+      console.log(match);
 
       if (match !== null) {
         break;
       }
     }
+    const pattern = RegExp(rxPattern);
+    const match = pattern.exec(message);
+
+    if (match == null) {
+      return res
+        .status(400)
+        .json(responseSchema(false, "Pattern not found", null));
+    }
+
+    const preExistingList = Bank.find({ bankName: bankName });
+
+    await preExistingList.forEach((document) => {
+      if (document.regexPattern) {
+        const patrn = RegExp(document.regexPattern);
+        const matched = patrn.exec(message);
+        if (matched !== null) {
+          return res.status(400).json(responseSchema(false, "Already Exists!"));
+        }
+      }
+    });
+
     const newBankTemplate = new Bank({
       bankName: bankName,
       propertyMap: propMap,
       regexPattern: rxPattern,
     });
-    const alreadyExists = await Bank.findOne({
-      $and: [{ regexPattern: rxPattern, bankName: bankName }],
-    });
-    if (alreadyExists) {
-      return res.status(400).json(responseSchema(false, "Already Exists!"));
-    }
+
     await newBankTemplate.save();
     return res
       .status(200)
-      .json(responseSchema(true, "bank template added", result));
+      .json(responseSchema(true, "bank template added", newBankTemplate));
   } catch (error) {
     return res.status(400).json(responseSchema(false, error.toString()));
   }
 };
 
-export { fetchAllTemplates, fetchTemplate, addTemplate };
+export { fetchAllTemplates, fetchTemplate, addTemplate, fetchListTemplate };
