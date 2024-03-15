905a95a722d65cf724edc75c514eeafe12112eb0
Vivek Pathak <vivekpathak2083@gmail.com>
2024-03-15 15:34:43 +0530

    bug fixed

--- know_app/server/src/helpers/messages/regexTemplate.js
+++ know_app/server/src/helpers/messages/regexTemplate.js
@@ -1,58 +1,51 @@
 import { config } from "dotenv";
 config();
-import { OpenAI } from "openai";
-
-const openai = new OpenAI({
-  apiKey: process.env.API_KEY, // Replace with your OpenAI API key
-});
+import { apiFunction } from "./apiFunction.js";
 
 async function regexTemplate(message) {
   const data = {
-    model: "gpt-3.5-turbo-0125",
+    model: process.env.model,
     messages: [
       {
-        role: "user",
-        content: `list me property and group number assigned in a map from the below string : Map<String, String> extractTransactionDetails(String text) {
-          RegExp pattern = RegExp(r'INR (\\d+\\.\\d+) credited to A/c no\\. XX(\\d+) on (\\d{2}-\\d{2}-\\d{2}) at (\\d{2}:\\d{2}:\\d{2}) IST\\. Info- UPI/P2A/(\\d+)/.*');
-          Match? match = pattern.firstMatch(text);
-          if (match != null) {
-            String amount = match.group(1)!;
-            String accountNumber = match.group(2)!;
-            String date = match.group(3)!;
-            String time = match.group(4)!;
-            String transactionId = match.group(5)!;
-            return {
-              'amount': amount,
-              'account_number': accountNumber,
-              'date': date,
-              'time': time,
-              'transaction_id': transactionId
-            };
-          } else {
-            return {};
-          }
-        }`,
+        role: "system",
+        content:
+          "donot explain. just provide answers as a code snippet. donot quote the output. remove the following if found at start of the result : r'  or RegExp(r' or (r'. remove the following if found at  end of the result : ') or '); ",
       },
-
+      {
+        role: "user",
+        content: `give me value stored in pattern variable in the below string : Map<String, String> extractTransactionDetails(String text) {
+              RegExp pattern = RegExp(r'INR (\\d+\\.\\d+) credited to A/c no\\. XX(\\d+) on (\\d{2}-\\d{2}-\\d{2}) at (\\d{2}:\\d{2}:\\d{2}) IST\\. Info- UPI/P2A/(\\d+)/.*');
+              Match? match = pattern.firstMatch(text);
+              if (match != null) {
+                String amount = match.group(1)!;
+                String accountNumber = match.group(2)!;
+                String date = match.group(3)!;
+                String time = match.group(4)!;
+                String transactionId = match.group(5)!;
+                return {
+                  'amount': amount,
+                  'account_number': accountNumber,
+                  'date': date,
+                  'time': time,
+                  'transaction_id': transactionId
+                };
+              } else {
+                return {};
+              }
+            }`,
+      },
       {
         role: "assistant",
-        content: `{ "amount" : 1, "accountNumber" : 2, "date" : 3, "time": 4, "transactionId" :5}`,
+        content: `INR (\\d+\\.\\d+) credited to A/c no\\. XX(\\d+) on (\\d{2}-\\d{2}-\\d{2}) at (\\d{2}:\\d{2}:\\d{2}) IST\\. Info- UPI/P2A/(\\d+)/.* `,
       },
       {
         role: "user",
-        content: `list me property and group number assigned in a map from the below string : ${message} `,
+        content: `give me value stored in pattern variable in the below string : ${message}`,
       },
     ],
   };
-  let result = "";
-  try {
-    const response = await openai.chat.completions.create(data);
-    result = response.choices[0].message.content;
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-  return null;
+
+  return apiFunction(data);
 }
 
 export { regexTemplate as regexPattern };
